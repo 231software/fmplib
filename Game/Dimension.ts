4019c0d1735d9739277267f8c8ad41615e69b00b
minimouse0 <473147334@qq.com>
2025-03-25 13:44:23 +0800

    refactor dimension api

--- Game/Dimension.ts
+++ Game/Dimension.ts
@@ -1,31 +1,22 @@
 import { TwoWayMap } from "../Tools";
 
-export enum FMPDefaultDimension{
-    NotDefault=-1,
-    Overworld=0,
-    Nether,
-    TheEnd
-}
-export const DefaultDimensionName=new TwoWayMap(new Map([
-    [-1,"非原版维度"],
-    [0,"主世界"],
-    [1,"下界"],
-    [2,"末地"]
-]))
 export class FMPDimension{
-    defaultDimension:FMPDefaultDimension
     name:string
-    constructor(defaultDimensionOrName:FMPDefaultDimension|string){
-        //通过默认维度生成fmp维度的方法
-        if(typeof defaultDimensionOrName!="string"){
-            this.defaultDimension=defaultDimensionOrName;
-            this.name=DefaultDimensionName.toRight(defaultDimensionOrName)
-            return;
-        }
-        this.name=defaultDimensionOrName;
-        this.defaultDimension=FMPDefaultDimension.NotDefault;
+    displayName:string
+    static createDimension(name:string){
+
     }
-    static getDefaultDimension(defaultDimension:FMPDefaultDimension):FMPDimension{
-        return new FMPDimension(defaultDimension);
+    /**
+     * 通过维度名获取维度实例  
+     * 必须通过此方法获得原版维度
+     * - 主世界：overworld
+     * - 下界：nether
+     * - 末地：the_end
+     * 其他维度的名字是在它被创建时被指定的那个
+     * @param name 维度名
+     * @returns 维度实例
+     */
+    static getDimension(name:string):FMPDimension{
+        return new FMPDimension
     }
 }
